139f44591c1f2ae3d1e60bf41c5de8712028ef5f
Sneekyboots <sneekyboots122@gmail.com>
2025-08-30 13:18:29 +0000

    We have updated the todo with different lists

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Rust_Mini_items
 Coool random projects hehehe of no use 
+
+## Cli based todo list
+first project is based on cli based todo list where u can write a command like this :
+command TODO  WRITE RUST PROGRAMS
+and it adds it to ur list 
+
+## First commit is based on CLI based Todo list 
+We have created a simple print the todo list .now on next commit we take input from the user 
+
+
--- todo-cli-tool/src/main.rs
+++ todo-cli-tool/src/main.rs
@@ -1,25 +1,80 @@
-
-#[derive (Debug)]
+use std::io;
+#[derive(Debug)]
 struct Task{
     title:String,
     done:bool
 }
 
-fn main() {
-   
 
-let mut tasks=Vec::new();
-tasks.push(Task{
-    title:String::from("THIS IS MY FIRST TASK"),
-    done:false
-});
-tasks.push(Task{
-    title:String::from("THIS IS MY SECOND TASK"),
-    done:true
-});
+fn main(){
+   let mut tasks:Vec<Task>=Vec::new();
 
+   loop{
+    let mut input=String::new();
+    println!("Enter the todo thing u want to add to list ");
+    io::stdin().read_line(&mut input).expect("Failed to take human input");
 
-for (i,task) in tasks.iter().enumerate(){
-    println!("{}:{} [{}]",i+1,task.title,if task.done {"done"} else {"TBD"})
-}
+    let input =input.trim();
+    if input=="exit"{
+        break;
+    }
+
+    let mut part= input.splitn(2,' ');
+    let command =part.next().unwrap();
+    let args=part.next();
+
+    match command {
+        "todo"=>{
+            if let Some(title)=args{
+                tasks.push(Task {title:title.to_string(),done:false});
+                println!("Added the task {}",title);
+
+            }
+            else{
+                    println!("Please provide a task title.");
+            }
+        },
+        "list"=>{
+            for (i,task) in tasks.iter().enumerate(){
+                println!("{}:{}[{}]",i+1,task.title,if task.done {"Done"}else {"TBD"});
+            }
+        },
+        "done"=>{
+        if let Some(num_str)=args{
+            if let Ok(num)=num_str.parse::<usize>(){
+                if num>0 &&num<=tasks.len(){
+                    tasks[num-1].done=true;
+                    println!("Marked task {} as done .",tasks[num-1].title);
+                }
+                else{
+                    println!("Please provide a valid number ");
+                }
+            }
+            else{
+                println!("This cant be parsed into a number")
+            }
+        }
+        },
+        "delete"=>{
+            if let Some(num_str)=args{
+                if let Ok(num)=num_str.parse::<usize>(){
+                    if num>0 &&num<=tasks.len(){
+                        let task=tasks.remove(num-1);
+                        println!("We have removed this tasks {}",task.title);
+                    }
+                    else{
+                        println!("Error the item was not found ")
+                    }
+                    
+                }
+                else{
+                    println!("Please provide a valid number.");
+                }
+            }
+        }
+        _=>{
+            println!("Unknown command :{}",command);
+        }
+    }
+   }
 }
